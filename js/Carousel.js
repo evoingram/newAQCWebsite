4e8c99fb8167096be97b3d331a43ac105590081c
Erica L. Ingram <inquiries@aquoco.co>
2019-12-20 21:00:28 -0800

    adjusted carousel pics to webp, added all webp images to directory

--- js/Carousel.js
+++ js/Carousel.js
@@ -1,23 +1,3 @@
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to write a function that creates the carousel component, you will find the HTML below.
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
-
-/* HTML:
-  <div class="carousel">
-    <div class="left-button"> < </div>
-    <img src="./assets/carousel/mountains.jpeg" />
-    <img src="./assets/carousel/computer.jpeg" />
-    <img src="./assets/carousel/trees.jpeg" />
-    <img src="./assets/carousel/turntable.jpeg" />
-    <div class="right-button"> > </div>
-  </div>
-*/
-
 function carouselMaker() {
   let carouselCTR = document.querySelector('.carousel-container');
   let divCarousel = carouselCTR.appendChild(document.createElement('div'));
@@ -25,17 +5,17 @@ function carouselMaker() {
   let divLeftButton = divCarousel.appendChild(document.createElement('div'));
   divLeftButton.classList.add('left-button');
   let imgTOA = divCarousel.appendChild(document.createElement('img'));
-  imgTOA.src = "img/table-of-authorities.jpg";
+  imgTOA.src = "img/table-of-authorities.webp";
   imgTOA.classList.add('imgCarousel');
   imgTOA.style.display = "block";
   imgTOA.id = "0";
   let imgTOAHLinks = divCarousel.appendChild(document.createElement('img'));
-  imgTOAHLinks.src = "img/TOA-HeadingLinks.jpg";
+  imgTOAHLinks.src = "img/TOA-HeadingLinks.webp";
   imgTOAHLinks.classList.add('imgCarousel');
   imgTOAHLinks.style.display = "none";
   imgTOAHLinks.id = "1";
   let imgWordIndex = divCarousel.appendChild(document.createElement('img'));
-  imgWordIndex.src = "img/word-index.jpg";
+  imgWordIndex.src = "img/word-index.webp";
   imgWordIndex.classList.add('imgCarousel');
   imgWordIndex.style.display = "none";
   imgWordIndex.id = "2";
